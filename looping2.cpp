3e3362405c3b28a1341510d9628873eb96403c6a
Januuu26 <b77286116@gmail.com>
2026-03-30 20:46:25 +0700

    membbuat nilai rerata dan status

--- looping2.cpp
+++ looping2.cpp
@@ -11,4 +11,12 @@ int main(){
     cout<<"Masukkan nilai Bahasa Inggris = ";
     cin>>niLB;
 
-    
+  rerata = (niLB+niLM)/2;
+
+    if (!(rerata < 60) )
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
