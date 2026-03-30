82902d5a090594d311ed1245feb330a56e6606fa
Januuu26 <b77286116@gmail.com>
2026-03-30 20:33:05 +0700

    membuat interger, nilai,rerata

---
+++ looping2.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float niLB,niLM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>niLM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>niLB;
+
+    rerata = (niLB+niLM)/2;
+
+    if (!(rerata < 60) )
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
