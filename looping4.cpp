a19e64de3f2389c933217e2f2cfe0f487a275152
Januuu26 <b77286116@gmail.com>
2026-03-30 20:39:23 +0700

    menghitung rerata nilai dan status

--- looping4.cpp
+++ looping4.cpp
@@ -10,4 +10,12 @@ int main(){
     cout<<"Masukkan nilai Bahasa Inggris = ";
     cin>>niLB;
 
-   
+    rerata = (niLB+niLM)/2;
+
+    if ((rerata >= 60) && (niLM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
