fb1472f7f412ee3af4782e34af7134b903a606ad
Januuu26 <b77286116@gmail.com>
2026-03-30 20:44:56 +0700

    membuat cout

--- looping2.cpp
+++ looping2.cpp
@@ -6,4 +6,9 @@ int main(){
     float niLB,niLM,rerata;
     string status;
 
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>niLM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>niLB;
+
     
