d8e13f3dde8673528cdda16a48e2558277b926cf
Januuu26 <b77286116@gmail.com>
2026-03-30 20:40:51 +0700

    membuat cout

--- looping3.cpp
+++ looping3.cpp
@@ -4,3 +4,8 @@ using namespace std;
 int main(){
     float niLB,niLM,rerata;
     string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>niLM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>niLB;
